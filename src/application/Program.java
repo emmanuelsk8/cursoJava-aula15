78cb5fef70334ab30726a4c8195731abb886edc1
Emmanuel H. Tamagnini <emmanuel.tamagnini@gmail.com>
2020-11-12 20:25:33 -0300

    Bad Solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import model.entities.Reservation;
 
-public class ProgramMRuim {
+public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		Scanner sc = new Scanner(System.in);
@@ -33,14 +33,10 @@ public class ProgramMRuim {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for updates");
-			}else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be check-in date");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("Error in reservation: "+error);
 			}else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println(reservation);
 			}
 		}
