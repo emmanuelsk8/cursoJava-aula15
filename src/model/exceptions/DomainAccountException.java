9a142cdbc830566666adc7900d54285db9b827fb
Emmanuel H. Tamagnini <emmanuel.tamagnini@gmail.com>
2020-11-12 22:15:26 -0300

    Exercício de Fixação

---
+++ src/model/exceptions/DomainAccountException.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class DomainAccountException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DomainAccountException(String msg) {
+		super(msg);
+	}
+}
